736f871b41722d9579946dd471c0cb0298b9f310
Robbie Averill <robbie@silverstripe.com>
2018-12-21 12:36:03 +0000

    Add tests for data columns GridField existence and basic formatting

--- tests/Page/CKANRegistryPageTest.php
+++ tests/Page/CKANRegistryPageTest.php
@@ -3,11 +3,25 @@
 namespace SilverStripe\CKANRegistry\Tests\Page;
 
 use SilverStripe\CKANRegistry\Page\CKANRegistryPage;
+use SilverStripe\CKANRegistry\Service\ResourceFieldPopulatorInterface;
+use SilverStripe\Core\Injector\Injector;
 use SilverStripe\Dev\SapphireTest;
+use SilverStripe\Forms\GridField\GridField;
+use Symbiote\GridFieldExtensions\GridFieldEditableColumns;
 
 class CKANRegistryPageTest extends SapphireTest
 {
     protected static $fixture_file = 'CKANRegistryPageTest.yml';
+
+    protected function setUp()
+    {
+        // Mock the field populator, in case an action we perform in a unit test tries to contact the mock API.
+        // Done before parent::setUp() so write hooks don't run during fixture population.
+        $populator = $this->createMock(ResourceFieldPopulatorInterface::class);
+        Injector::inst()->registerService($populator, ResourceFieldPopulatorInterface::class);
+
+        parent::setUp();
+    }
 
     public function testHasItemsPerPageSetting()
     {
@@ -15,5 +29,55 @@ class CKANRegistryPageTest extends SapphireTest
         $page = $this->objFromFixture(CKANRegistryPage::class, 'animal_centers');
 
         $this->assertNotNull($page->getSettingsFields()->fieldByName('Root.Settings.ItemsPerPage'));
+    }
+
+    public function testColumnsGridFieldHasUniqueClassName()
+    {
+        /** @var CKANRegistryPage $page */
+        $page = $this->objFromFixture(CKANRegistryPage::class, 'animal_centers');
+
+        $field = $page->getCMSFields()->fieldByName('Root.Data.DataColumns');
+        $this->assertInstanceOf(GridField::class, $field, 'Columns GridField was not found in fields');
+        $this->assertTrue($field->hasClass('ckan-columns'), 'Columns GridField should have unique class');
+    }
+
+    public function testNoDataColumnsGridFieldGetsAddedWhenNoResourceIsDefined()
+    {
+        $page = new CKANRegistryPage();
+        $page->write();
+
+        $field = $page->getCMSFields()->fieldByName('Root.Data.DataColumns');
+        $this->assertNull($field, 'Columns should not be added until a resource is defined');
+    }
+
+    public function testHasEditableColumnsComponent()
+    {
+        /** @var CKANRegistryPage $page */
+        $page = $this->objFromFixture(CKANRegistryPage::class, 'animal_centers');
+
+        /** @var GridField $field */
+        $field = $page->getCMSFields()->fieldByName('Root.Data.DataColumns');
+        $this->assertInstanceOf(GridField::class, $field, 'Columns GridField was not found in fields');
+        $this->assertNotNull($field->getConfig()->getComponentByType(GridFieldEditableColumns::class));
+    }
+
+    public function testEditableColumnsHaveCorrectDisplayFields()
+    {
+        /** @var CKANRegistryPage $page */
+        $page = $this->objFromFixture(CKANRegistryPage::class, 'animal_centers');
+
+        /** @var GridField $field */
+        $field = $page->getCMSFields()->fieldByName('Root.Data.DataColumns');
+        $this->assertInstanceOf(GridField::class, $field, 'Columns GridField was not found in fields');
+
+        /** @var GridFieldEditableColumns $component */
+        $component = $field->getConfig()->getComponentByType(GridFieldEditableColumns::class);
+        $this->assertNotNull($component, 'Component was not found in the GridField');
+
+        $displayFields = $component->getDisplayFields($field);
+        $this->assertEquals([
+            'ShowInSummaryView' => _t('SilverStripe\\CKANRegistry\\Page\\CKANRegistryPage.IN_RESULTS', 'In Results'),
+            'ShowInDetailView' => _t('SilverStripe\\CKANRegistry\\Page\\CKANRegistryPage.IN_DETAIL', 'In Detail'),
+        ], $displayFields, 'Correct display fields were not found');
     }
 }
