9da8232bc6da336a5c88bbeb5a6c965cf44eb624
Robbie Averill <robbie@silverstripe.com>
2019-01-10 17:32:45 +0100

    FIX Ensure fields and filters are actually deleted rather than just unassociated

--- tests/Model/ResourceTest.php
+++ tests/Model/ResourceTest.php
@@ -3,6 +3,8 @@
 namespace SilverStripe\CKANRegistry\Tests\Model;
 
 use SilverStripe\CKANRegistry\Model\Resource;
+use SilverStripe\CKANRegistry\Model\ResourceField;
+use SilverStripe\CKANRegistry\Model\ResourceFilter;
 use SilverStripe\CKANRegistry\Service\ResourcePopulator;
 use SilverStripe\CKANRegistry\Service\ResourcePopulatorInterface;
 use SilverStripe\Core\Injector\Injector;
@@ -45,5 +47,21 @@ class ResourceTest extends SapphireTest
             $resource->Filters(),
             'Changing identifier should clear filters (note that a default filter is added again as well)'
         );
+    }
+
+    public function testFieldsAndFiltersAreDeletedRatherThanJustUnassociated()
+    {
+        /** @var Resource $resource */
+        $resource = $this->objFromFixture(Resource::class, 'teachers');
+
+        $this->assertGreaterThan(0, $resource->Fields()->count(), 'Fixtures should load relationships');
+        $this->assertGreaterThan(0, $resource->Filters()->count(), 'Fixtures should load relationships');
+
+        // Change identifier, relationships should be deleted from the database
+        $resource->Identifier = 'something-different';
+        $resource->write();
+
+        $this->assertCount(0, ResourceField::get(), 'Resource fields should be deleted');
+        $this->assertCount(1, ResourceFilter::get(), 'Resource filters should be deleted (one gets added)');
     }
 }
