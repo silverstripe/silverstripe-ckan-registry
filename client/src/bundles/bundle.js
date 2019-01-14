83b87435f38675b35f95e35290da0d8abd94823c
Robbie Averill <robbie@silverstripe.com>
2019-01-14 17:06:23 +0100

    NEW TextFilter and DropdownFilter components
    * Create storybook examples, "with label" & "without label"
    * Exclude storybook files from code coverage reports
    * Remove configurable placeholder and replace with a label provided via props
    * Use Reactstrap form components for input rendering
    
    In order to allow filtering of CKAN resource data displayed on a
    SilverStripe site, we need to introduce React based components to work
    with Griddle (the chosen library for displaying the data). These accept
    input, and trigger a call back for updating, which is passed in as a
    prop.

--- client/src/bundles/bundle.js
+++ client/src/bundles/bundle.js
@@ -1,1 +1,4 @@
+// Components that are only used on the frontend view
 require('legacy/CKANRegistryDisplay-loader.js');
+require('components/CKANDropdownFilter');
+require('components/CKANTextFilter');
---
+++ client/src/components/tests/CKANTextFilter-test.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15.4/build/index';
+import CKANTextFilter from '../CKANTextFilter';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('CKANTextFilter', () => {
+  describe('render()', () => {
+    it('renders a text input', () => {
+      const wrapper = mount(
+        <CKANTextFilter
+          id="123"
+        />
+      );
+
+      expect(wrapper.find('input[type="text"]').length).toBe(1);
+    });
+
+    it('renders a label for the input field when provided', () => {
+      const wrapper = mount(
+        <CKANTextFilter
+          id="123"
+          label="Search things"
+        />
+      );
+
+      expect(wrapper.find('label').length).toBe(1);
+      expect(wrapper.find('label').text()).toContain('Search things');
+    });
+
+    it('renders a hidden input field', () => {
+      const wrapper = mount(
+        <CKANTextFilter
+          id="123"
+        />
+      );
+
+      expect(wrapper.find('input[type="hidden"]').length).toBe(1);
+    });
+
+    it('adds the provided extraClass values', () => {
+      const wrapper = mount(
+        <CKANTextFilter
+          id="123"
+          extraClass="foo bar baz"
+        />
+      );
+
+      expect(wrapper.find('div.foo.bar.baz').length).toBe(1);
+    });
+  });
+});
