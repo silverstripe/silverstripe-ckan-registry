d8d20d1ed330884a9fe0828a77917fce371cb90b
Robbie Averill <robbie@silverstripe.com>
2019-01-16 15:39:19 +0100

    NEW Add frontend DropdownFilter to provide a single select choice from a set of columns

--- client/src/bundles/bundle-frontend.js
+++ client/src/bundles/bundle-frontend.js
@@ -1,2 +1,3 @@
 // Components that are only used on the frontend view
+require('components/CKANDropdownFilter');
 require('components/CKANTextFilter');
---
+++ client/src/components/CKANDropdownFilter.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { FormGroup, Input, Label } from 'reactstrap';
+
+/**
+ * A dropdown filter is used to filter a CKAN dataset by a specific column, using
+ * the selected value from the dropdown filter.
+ *
+ * E.g. "filter 'school_name' by the selected value"
+ */
+const CKANDropdownFilter = (props) => {
+  const {
+    allColumns,
+    columns,
+    extraClass,
+    id,
+    label,
+    selections,
+  } = props;
+
+  const classes = classnames(extraClass, 'form-group', 'ckan-registry__dropdown-filter');
+  const inputId = `DropdownFilter_${id}_Search`;
+
+  return (
+    <FormGroup className={classes}>
+      { label && <Label for={inputId}>{label}</Label> }
+
+      <Input
+        id={inputId}
+        type="select"
+        name={`DropdownFilter[${id}][Search]`}
+      >
+        { selections.map(value => <option key={value}>{ value }</option>) }
+      </Input>
+
+      <Input
+        type="hidden"
+        name={`DropdownFilter[${id}][Columns]`}
+        value={JSON.stringify(columns)}
+      />
+      <Input
+        type="hidden"
+        name={`DropdownFilter[${id}][AllColumns]`}
+        value={allColumns ? 1 : 0}
+      />
+    </FormGroup>
+  );
+};
+
+CKANDropdownFilter.propTypes = {
+  id: PropTypes.string.isRequired,
+  selections: PropTypes.arrayOf(
+    PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+  ).isRequired,
+  allColumns: PropTypes.bool,
+  columns: PropTypes.arrayOf(PropTypes.string),
+  extraClass: PropTypes.string,
+  label: PropTypes.string,
+};
+
+CKANDropdownFilter.defaultProps = {
+  allColumns: false,
+  columns: [],
+  extraClass: '',
+  label: '',
+};
+
+export default CKANDropdownFilter;
