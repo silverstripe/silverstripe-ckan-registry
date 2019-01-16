7cfce99dabbf674d2414b8e5a6e8f27ceccaad74
Robbie Averill <robbie@silverstripe.com>
2019-01-16 14:43:39 +0100

    Update TextFilter story book to include a "with label" and "without label" example

--- client/src/components/tests/CKANTextFilter-story.js
+++ client/src/components/tests/CKANTextFilter-story.js
@@ -4,39 +4,20 @@ import { storiesOf } from '@storybook/react';
 import CKANTextFilter from '../CKANTextFilter';
 
 storiesOf('CKAN Registry/Text Filter', module)
-  .add('All columns', () => (
+  .add('With label', () => (
     <div>
       <CKANTextFilter
         id="1"
-        columns={[]}
-        label="Search all fields"
-      />
-    </div>
-  ))
-  .add('One column', () => (
-    <div>
-      <CKANTextFilter
-        id="2"
-        columns={['topic']}
-        label="Search by topic"
-      />
-    </div>
-  ))
-  .add('Two columns', () => (
-    <div>
-      <CKANTextFilter
-        id="3"
         columns={['topic', 'organisation']}
         label="Search by topic or organisation"
       />
     </div>
   ))
-  .add('Three columns', () => (
+  .add('Without label', () => (
     <div>
       <CKANTextFilter
-        id="4"
-        columns={['topic', 'organisation', 'department']}
-        label="Search by topic, organisation, or department"
+        id="2"
+        columns={[]}
       />
     </div>
   ));
