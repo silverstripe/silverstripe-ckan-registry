b282a0d4bdad7368d0fd56859c092a2f617f5509
Guy Sartorelli <guy.sartorelli@silverstripe.com>
2023-08-18 13:45:32 +1200

    ENH Update translations

--- client/lang/eo.js
+++ client/lang/eo.js
@@ -6,14 +6,14 @@ if (typeof(ss) === 'undefined' || typeof(ss.i18n) === 'undefined') {
   }
 } else {
   ss.i18n.addDictionary('eo', {
+    "CKANRegistryDetailView.BACK": "Retro",
+    "CKANRegistryDetailView.LOADING": "Ŝargas...",
+    "CKANRegistryDetailView.NO_RESULTS": "Ne trovis rezultojn!",
+    "CKANRegistryDisplay.DOWNLOAD": "Eksporti rezultojn al CSV",
     "CKANResourceLocatorField.CHANGES_WARNING": "Bonvolu noti: ŝanĝi la datumfontan URL aŭ rimedon, vakigos ĉiujn ekzistantajn kolumnojn kaj filtrilojn kiam vi konservas la paĝon.",
     "CKANResourceLocatorField.DATA_SOURCE_URL": "Datumfonta URL",
     "CKANResourceLocatorField.INVALID_DATASET_URL": "La donita datumfonta URL ŝajnas ne esti valida CKAN-datumfonto.",
     "CKANResourceLocatorField.INVALID_RESOURCE_SELECTION": "Datuma konservejo ne disponeblas por la elektita rimedo.",
-    "CKANResourceLocatorField.RESOURCE_NAME": "Nomo de rimedo",
-    "CKANRegistryDetailView.BACK": "Retro",
-    "CKANRegistryDetailView.LOADING": "Ŝargas...",
-    "CKANRegistryDetailView.NO_RESULTS": "Ne trovis rezultojn!",
-    "CKANRegistryDisplay.DOWNLOAD": "Eksporti rezultojn al CSV"
+    "CKANResourceLocatorField.RESOURCE_NAME": "Nomo de rimedo"
 });
 }
